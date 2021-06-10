0a51f333f1166c4b49cfbc1169e5dfdf51e83c90
amv1017 <amv1017@mail.ru>
2021-06-10 22:11:45 +0300

    initialized

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../public/src/components/layout"
+
+function About() {
+    return (
+        <Layout>
+            About Page
+        </Layout>
+    )
+}
+
+export default About
